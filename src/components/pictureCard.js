7645c190c49104a57bd52b077c757b2cf516ff58
maddyzablocki <zablockimadison@gmail.com>
2019-12-12 20:24:11 -0500

    MVP day two done

--- src/components/pictureCard.js
+++ src/components/pictureCard.js
@@ -1,25 +1,28 @@
 import React from "react";
+import { Card, CardBody, CardImg } from "reactstrap";
+import {MyCardTitle} from "./styles";
+import {MyText} from "./styles";
 
-const PictureCard = (props) => {
-    return(
-        <div>
-            <h1>
-                {props.title}
-            </h1>
-            <h2>
-                {props.date}
-            </h2>
-            <img src = {props.url} alt="NASA"/>
-            <div>
-                <h3>
-                    Details
-                </h3>
-                <p>
-                    {props.explanation}
-                </p>
-            </div>
-        </div>
+
+const PictureCard = props => {
+    return (
+      <div>
+        <Card color="dark">
+          <CardBody>
+            <MyCardTitle color="primary">{props.title}</MyCardTitle>
+            <h2> {props.date} </h2>
+            <CardImg
+            top
+            width="100%"
+            src={props.url}
+            alt="NASA Image Of The Day"
+            style={{ maxWidth: "500px" }}
+          />
+          <MyText> {props.explanation} </MyText>
+          </CardBody>
+        </Card>
+      </div>
     );
-};
+  };
 
-export default PictureCard;
+  export default PictureCard;
